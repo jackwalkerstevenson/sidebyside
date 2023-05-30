2735eced09668bf82b121fa61931834d88eccdf6
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2023-05-30 00:40:14 -0700

    Added plotting code and ordered entries by mean score

--- score.R
+++ score.R
@@ -1,19 +1,26 @@
 # sidebyside contest scoring
 # Jack Stevenson, 2023-06
 library(tidyverse)
+library(ggplot2)
 library(readxl)
+library(ggprism)
+library(ggdist)
 source("import.R")
 # import score data from spreadsheet
 input_filename <- "test.xlsx"
 raw_data <- import_scores(input_filename)
-scored_data <- raw_data |>
+scores <- raw_data |>
   dplyr::group_by(judge) |>
   mutate(z_score = as.vector(scale(score))) |>
   mutate(z_score = tidyr::replace_na(z_score, 0)) |>
-  ungroup()
-overall_scores <- scored_data |>
-  dplyr::group_by(entry) |>
-  dplyr::summarize(
-    mean_z_score = mean(z_score),
-    sd_z_score = stats::sd(z_score)
-  )
+  ungroup() |>
+  mutate(entry = fct_reorder(entry, z_score, mean))
+
+scores |>
+  ggplot(aes(x = z_score, y = entry)) +
+  theme_prism() +
+  geom_boxplot(width = 0.3) +
+  geom_point(stat = "summary", fun = "mean", size = 4) +
+  labs(x = "Z score",
+       y = "entry",
+       title = "Standardized scores")
