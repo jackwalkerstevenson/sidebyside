56139a9e7b2772d746a5bca51c39be24d6ebf61e
Jack Stevenson <2307047+jackwalkerstevenson@users.noreply.github.com>
2023-05-29 23:37:31 -0700

    Added import function to read custom-formatted import template data and tidy

---
+++ import.R
@@ -0,0 +1,16 @@
+#' Import score data from an Excel spreadsheet.
+#'
+#' Input spreadsheet is not tidy. See template for format: Columns are "judge"
+#' and then entry IDs. Each row is a judge name and associated scores scores.
+#' Judge names are uniqued by appending a digit if not already unique.
+#' @param input_filename Path to the file to import.
+#'
+#' @return A dataframe containing tidy data
+#' @export
+import_scores <- function(input_filename){
+  readxl::read_excel(input_filename) |>
+    dplyr::mutate(judge = make.unique(judge, sep = "_")) |>
+    tidyr::pivot_longer(!judge,
+                        names_to = "entry",
+                        values_to = "score")
+}
